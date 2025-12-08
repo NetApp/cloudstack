2e8f8eee6fdd0573c51555f2e754ba38f731ccfd
sandeeplocharla <85344604+sandeeplocharla@users.noreply.github.com>
2025-12-08 11:12:08 +0530

    CSTACKEX-50: Disable, Re-Enable, Delete Storage pool and Enter, Exit Maintenance mode (#20)
    * CSTACKEX-50: Disable, Re-Enable, Delete Storage pool and Enter, Exit Storage pool workflows
    
    * CSTACKEX-50: Changes for selecting aggregate, retrieving ip interface and setting host and path
    
    * CSTACKEX-50: Fixed some issues seen while testing
    
    * CSTACKEX-50: Fixed few EOF lint issues
    
    * CSTACKEX-50: Fixed few lint issues
    
    * CSTACKEX-50: Included changes for Delete storage pool and modified some methods according to the latest design
    
    * CSTACKEX-050: Rebased the code with the latest in master
    
    * CSTACKEX-050: Fixed lint issues
    
    * CSTACKEX-050: Added some missing changes
    
    ---------
    
    Co-authored-by: Locharla, Sandeep <Sandeep.Locharla@netapp.com>

--- plugins/storage/volume/ontap/src/main/java/org/apache/cloudstack/storage/utils/Constants.java
+++ plugins/storage/volume/ontap/src/main/java/org/apache/cloudstack/storage/utils/Constants.java
@@ -19,6 +19,7 @@
 
 package org.apache.cloudstack.storage.utils;
 
+
 public class Constants {
 
     public static final String NFS = "nfs";
@@ -28,7 +29,10 @@ public class Constants {
     public static final String SVM_NAME = "svmName";
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
+    public static final String DATA_LIF = "dataLIF";
     public static final String MANAGEMENT_LIF = "managementLIF";
+    public static final String VOLUME_NAME = "volumeName";
+    public static final String VOLUME_UUID = "volumeUUID";
     public static final String IS_DISAGGREGATED = "isDisaggregated";
     public static final String RUNNING = "running";
 
@@ -40,24 +44,41 @@ public class Constants {
     public static final String JOB_FAILURE = "failure";
     public static final String JOB_SUCCESS = "success";
 
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+
     // Query params
     public static final String NAME = "name";
     public static final String FIELDS = "fields";
     public static final String AGGREGATES = "aggregates";
     public static final String STATE = "state";
+    public static final String DATA_NFS = "data_nfs";
+    public static final String DATA_ISCSI = "data_iscsi";
+    public static final String IP_ADDRESS = "ip.address";
+    public static final String SERVICES = "services";
+    public static final String RETURN_RECORDS = "return_records";
 
     public static final int JOB_MAX_RETRIES = 100;
     public static final int CREATE_VOLUME_CHECK_SLEEP_TIME = 2000;
 
-    public static final String PATH_SEPARATOR = "/";
+    public static final String SLASH = "/";
     public static final String EQUALS = "=";
     public static final String SEMICOLON = ";";
     public static final String COMMA = ",";
+    public static final String HYPHEN = "-";
 
     public static final String VOLUME_PATH_PREFIX = "/vol/";
 
+    public static final String ONTAP_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9_]*$";
     public static final String KVM = "KVM";
 
     public static final String HTTPS = "https://";
-
+    public static final String SVM_DOT_NAME = "svm.name";
+    public static final String LUN_DOT_NAME = "lun.name";
+    public static final String IQN = "iqn";
+    public static final String LUN_DOT_UUID = "lun.uuid";
+    public static final String IGROUP_DOT_NAME = "igroup.name";
+    public static final String IGROUP_DOT_UUID = "igroup.uuid";
+    public static final String UNDERSCORE = "_";
+    public static final String CS = "cs";
 }
