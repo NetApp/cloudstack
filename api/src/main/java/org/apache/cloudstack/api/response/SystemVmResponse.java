6b51e3c1827bbed8ced0350c5230a2a0247982d7
Erik Böck <89930804+erikbocks@users.noreply.github.com>
2026-01-27 08:59:06 -0300

    Implement SSVM storage network IP to API response and GUI details tab (#11979)
    * Implement SSVM storage network IP to API response and GUI details tab
    
    * remove network mention from attribute name
    
    * remove network from serialized name
    
    * fix parameter name in the UI

--- api/src/main/java/org/apache/cloudstack/api/response/SystemVmResponse.java
+++ api/src/main/java/org/apache/cloudstack/api/response/SystemVmResponse.java
@@ -126,6 +126,10 @@ public class SystemVmResponse extends BaseResponseWithAnnotations {
     @Param(description = "the public netmask for the system VM")
     private String publicNetmask;
 
+    @SerializedName("storageip")
+    @Param(description = "the ip address for the system VM on the storage network")
+    private String storageIp;
+
     @SerializedName("templateid")
     @Param(description = "the template ID for the system VM")
     private String templateId;
@@ -353,6 +357,14 @@ public class SystemVmResponse extends BaseResponseWithAnnotations {
 
     public void setPublicNetmask(String publicNetmask) {
         this.publicNetmask = publicNetmask;
+    }
+
+    public String getStorageIp() {
+        return storageIp;
+    }
+
+    public void setStorageIp(String storageIp) {
+        this.storageIp = storageIp;
     }
 
     public String getTemplateId() {
