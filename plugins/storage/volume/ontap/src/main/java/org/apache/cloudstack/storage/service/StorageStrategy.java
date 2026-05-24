0ceb7ed58948a2cc8e714913428820277c5bdf52
Gupta, Surya <Surya.Gupta@netapp.com>
2026-05-24 20:33:14 +0530

    [CSTACKEX-129] NFS3 Primary storage-pool is not failing even if NFS3 protocol is not enabled at the storage VM

--- plugins/storage/volume/ontap/src/main/java/org/apache/cloudstack/storage/service/StorageStrategy.java
+++ plugins/storage/volume/ontap/src/main/java/org/apache/cloudstack/storage/service/StorageStrategy.java
@@ -109,31 +109,31 @@ public abstract class StorageStrategy {
             logger.info("Fetching the SVM details...");
             Map<String, Object> queryParams = Map.of(OntapStorageConstants.NAME, svmName, OntapStorageConstants.FIELDS, OntapStorageConstants.AGGREGATES +
                     OntapStorageConstants.COMMA + OntapStorageConstants.STATE +
-                    OntapStorageConstants.COMMA +  OntapStorageConstants.NFS + OntapStorageConstants.COMMA + OntapStorageConstants.ISCSI);
+                    OntapStorageConstants.COMMA +  OntapStorageConstants.NFS_ENABLED + OntapStorageConstants.COMMA + OntapStorageConstants.ISCSI_ENABLED);
             OntapResponse<Svm> svms = svmFeignClient.getSvmResponse(queryParams, authHeader);
             if (svms != null && svms.getRecords() != null && !svms.getRecords().isEmpty()) {
                 svm = svms.getRecords().get(0);
             } else {
                 logger.error("No SVM found on the ONTAP cluster by the name" + svmName + ".");
-                return false;
+                throw new CloudRuntimeException("No SVM found on the ONTAP cluster by the name" + svmName + ".");
             }
 
             logger.info("Validating SVM state and protocol settings...");
             if (!Objects.equals(svm.getState(), OntapStorageConstants.RUNNING)) {
                 logger.error("SVM " + svmName + " is not in running state.");
-                return false;
+                throw new CloudRuntimeException("SVM " + svmName + " is not in running state.");
             }
             if (Objects.equals(storage.getProtocol(), ProtocolType.NFS3) && !svm.getNfsEnabled()) {
                 logger.error("NFS protocol is not enabled on SVM " + svmName);
-                return false;
+                throw new CloudRuntimeException("NFS protocol is not enabled on SVM " + svmName);
             } else if (Objects.equals(storage.getProtocol(), ProtocolType.ISCSI) && !svm.getIscsiEnabled()) {
                 logger.error("iSCSI protocol is not enabled on SVM " + svmName);
-                return false;
+                throw new CloudRuntimeException("iSCSI protocol is not enabled on SVM " + svmName);
             }
             List<Aggregate> aggrs = svm.getAggregates();
             if (aggrs == null || aggrs.isEmpty()) {
                 logger.error("No aggregates are assigned to SVM " + svmName);
-                return false;
+                throw new CloudRuntimeException("No aggregates are assigned to SVM " + svmName);
             }
             for (Aggregate aggr : aggrs) {
                 logger.debug("Found aggregate: " + aggr.getName() + " with UUID: " + aggr.getUuid());
