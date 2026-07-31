705151dbea756c49711884f9338745739f930590
sandeeplocharla <slocharl@netapp.com>
2026-07-31 08:17:03 +0530

    Corrected a comment

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStoragePoolManager.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStoragePoolManager.java
@@ -417,7 +417,7 @@ public class KVMStoragePoolManager {
         if (type == StoragePoolType.NetworkFilesystem) {
             _haMonitor.removeStoragePool(uuid);
         }
-        boolean deleteStatus = adaptor.deleteStoragePool(uuid);;
+        boolean deleteStatus = adaptor.deleteStoragePool(uuid);
         synchronized (_storagePools) {
             _storagePools.remove(uuid);
         }
@@ -427,7 +427,7 @@ public class KVMStoragePoolManager {
     public boolean deleteStoragePool(StoragePoolType type, String uuid, Map<String, String> details) {
         StorageAdaptor adaptor = getStorageAdaptor(type);
         // For NetworkFilesystem, libvirt will take care of unmounting the nfs mount. If nfs mount has been removed before libvirt's pool
-        // delete, libvirt will throw an error. So, we need to remove the pool from HA monitor before deleting the pool.
+        // delete, libvirt will throw an error.
         boolean deleteStatus = adaptor.deleteStoragePool(uuid, details);
         if (type == StoragePoolType.NetworkFilesystem) {
             _haMonitor.removeStoragePool(uuid);
