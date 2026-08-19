affb5575f8d0c9f4be21306417c6516933364e65
Srivastava, Piyush <Piyush.Srivastava@netapp.com>
2026-08-19 12:34:27 +0530

    bugfix/CSTACKEX-235: Updated job timing to be uniform across ONTAP

--- plugins/storage/volume/ontap/src/main/java/org/apache/cloudstack/storage/service/StorageStrategy.java
+++ plugins/storage/volume/ontap/src/main/java/org/apache/cloudstack/storage/service/StorageStrategy.java
@@ -316,7 +316,7 @@ public abstract class StorageStrategy {
             }
             String jobUUID = jobResponse.getJob().getUuid();
 
-            Boolean jobSucceeded = jobPollForSuccess(jobUUID,10, 1000);
+            Boolean jobSucceeded = jobPollForSuccess(jobUUID,30, 2000);
             if (!jobSucceeded) {
                 logger.error("Volume creation job failed for volume: " + volumeName);
                 throw new CloudRuntimeException("Volume creation job failed for volume: " + volumeName);
@@ -399,7 +399,7 @@ public abstract class StorageStrategy {
         String authHeader = OntapStorageUtils.generateAuthHeader(storage.getUsername(), storage.getPassword());
         try {
             JobResponse jobResponse = volumeFeignClient.deleteVolume(authHeader, volume.getUuid());
-            Boolean jobSucceeded = jobPollForSuccess(jobResponse.getJob().getUuid(), 10, 1000);
+            Boolean jobSucceeded = jobPollForSuccess(jobResponse.getJob().getUuid(), 30, 2000);
             if (!jobSucceeded) {
                 logger.error("Volume deletion job failed for volume: " + volume.getName());
                 throw new CloudRuntimeException("Volume deletion job failed for volume: " + volume.getName());
